95b546e4c44b832a465cd1c6955d0d0bff61e980
Mykle <themykle@imykle.com>
2017-07-05 20:16:28 -0400

    Add files via upload

---
+++ MMM-NatGeo.js
@@ -0,0 +1,147 @@
+/* Magic Mirror
+ * Module: MMM-NatGeo
+ *
+ * By Mykle1
+ *
+ */
+Module.register("MMM-NatGeo", {
+
+    // Module config defaults.
+    defaults: {
+        useHeader: true,             // False if you don't want a header      
+        header: "National Geographic",       // Any text you want. useHeader must be true
+        maxWidth: "350px",
+        animationSpeed: 3000,        // fade speed
+        initialLoadDelay: 4250,
+        retryDelay: 2500,
+        rotateInterval: 30 * 1000,   // 30 seconds
+        updateInterval: 30 * 60 * 1000,
+
+    },
+
+    getStyles: function() {
+        return ["MMM-NatGeo.css"];
+    },
+
+    start: function() {
+        Log.info("Starting module: " + this.name);
+
+        requiresVersion: "2.1.0",
+
+            //  Set locale.
+        this.url = "https://newsapi.org/v1/articles?source=national-geographic&sortBy=top&apiKey=bdc660fcecc745228206b1865e7d0612";
+        this.NatGeo = [];
+        this.activeItem = 0;
+        this.rotateInterval = null;
+        this.scheduleUpdate();
+    },
+
+    getDom: function() {
+
+        var wrapper = document.createElement("div");
+        wrapper.className = "wrapper";
+        wrapper.style.maxWidth = this.config.maxWidth;
+
+        if (!this.loaded) {
+            wrapper.innerHTML = "NatGeo Presents . . .";
+            wrapper.classList.add("bright", "light", "small");
+            return wrapper;
+        }
+
+        if (this.config.useHeader != false) {
+            var header = document.createElement("header");
+            header.classList.add("xsmall", "bright", "header");
+            header.innerHTML = this.config.header;
+            wrapper.appendChild(header);
+        }
+		
+		
+	//	My data isn't rotating! Why? I dont know. I'm an idiot
+		
+		var NatGeoKeys = Object.keys(this.NatGeo);
+        if (NatGeoKeys.length > 0) {
+            if (this.activeItem >= NatGeoKeys.length) {
+                this.activeItem = 0;
+            }
+            var NatGeo = this.NatGeo[NatGeoKeys[this.activeItem]];
+		
+		
+
+    //    var NatGeo = this.NatGeo;
+
+        var top = document.createElement("div");
+        top.classList.add("list-row");
+		
+		var NatGeo = this.NatGeo;
+
+
+        // The title
+        var title = document.createElement("div");
+        var NatGeoTitle = document.createElement("p");
+        NatGeoTitle.classList.add("small", "bright");
+        NatGeoTitle.innerHTML = NatGeo[0].title; // the first object is NatGeo[0];
+        title.appendChild(NatGeoTitle);
+        wrapper.appendChild(title);
+
+
+
+        // The picture
+        var pic = document.createElement("div");
+        var img = document.createElement("img");
+        img.classList.add("photo");
+        img.src = NatGeo[0].urlToImage; // the first object is NatGeo[0]
+        pic.appendChild(img);
+        wrapper.appendChild(pic);
+
+
+        
+
+       // The description
+        var description = document.createElement("div");
+        var NatGeoDescription = document.createElement("p");
+        NatGeoDescription.classList.add("xsmall", "bright", "description");
+        NatGeoDescription.innerHTML = NatGeo[0].description; // the first object is NatGeo[0];
+        description.appendChild(NatGeoDescription);
+        wrapper.appendChild(description);
+
+		}
+        return wrapper;
+    },
+
+
+    processNatGeo: function(data) {
+        this.today = data.Today;
+        this.NatGeo = data; // NatGeo = objects
+        this.loaded = true;
+    },
+
+    scheduleCarousel: function() {
+        console.log("Carousel of NatGeo fucktion!");
+        this.rotateInterval = setInterval(() => {
+            this.activeItem++;
+            this.updateDom(this.config.animationSpeed);
+        }, this.config.rotateInterval);
+    },
+
+    scheduleUpdate: function() {
+        setInterval(() => {
+            this.getNatGeo();
+        }, this.config.updateInterval);
+        this.getNatGeo(this.config.initialLoadDelay);
+    },
+
+    getNatGeo: function() {
+        this.sendSocketNotification('GET_NATGEO', this.url);
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === "NATGEO_RESULT") {
+            this.processNatGeo(payload);
+            if (this.rotateInterval == null) {
+                this.scheduleCarousel();
+            }
+            this.updateDom(this.config.animationSpeed);
+        }
+        this.updateDom(this.config.initialLoadDelay);
+    },
+});
---
+++ node_helper.js
@@ -0,0 +1,37 @@
+/* Magic Mirror
+ * Module: MMM-NatGeo
+ *
+ * By Mykle1
+ *
+ */
+const NodeHelper = require('node_helper');
+const request = require('request');
+
+
+
+module.exports = NodeHelper.create({
+
+    start: function() {
+        console.log("Starting node_helper for: " + this.name);
+    },
+
+    getNatGeo: function(url) {
+        request({
+            url: url,
+            method: 'GET'
+        }, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                var result = JSON.parse(body).articles;
+	//			var result = JSON.parse(body);
+	//			console.log(response.statusCode + result);
+                this.sendSocketNotification('NATGEO_RESULT', result);
+            }
+        });
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === 'GET_NATGEO') {
+            this.getNatGeo(payload);
+        }
+    }
+});
